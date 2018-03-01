4c8b248124e4adf4b9166f01f4f791cd3f958e07
cailinguang <user_cai@126.com>
2018-03-01 20:03:10 +0800

    commit

--- src/com/vw/isms/standard/repository/SimpleJdbcUpdate.java
+++ src/com/vw/isms/standard/repository/SimpleJdbcUpdate.java
@@ -1,6 +1,8 @@
 package com.vw.isms.standard.repository;
 
 import java.util.Map;
+import java.util.logging.Logger;
+
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.util.StringUtils;
@@ -8,6 +10,8 @@ import org.springframework.util.StringUtils;
 public class SimpleJdbcUpdate
   extends SimpleJdbcOperation
 {
+    private final Logger logger = Logger.getLogger(getClass().getName());
+
   public void update(NamedParameterJdbcTemplate template)
     throws DataAccessException
   {
@@ -21,6 +25,9 @@ public class SimpleJdbcUpdate
     builder.append(" WHERE ");
     builder.append(StringUtils.collectionToDelimitedString(GetNamedParameters(this.keys.keySet()), " AND "));
     this.columns.putAll(this.keys);
-    template.update(builder.toString(), this.columns);
+    int count = template.update(builder.toString(), this.columns);
+    if(count==0){
+        logger.info("update 0 data!");
+    }
   }
 }
